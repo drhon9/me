2f01080cb436a3b767acc070f6d30236dc2bad4a
unknown <antaresax@gmail.com>
2019-07-20 17:01:24 +0900

    unpper case

--- js/Dr.Api.js
+++ js/Dr.Api.js
@@ -4,10 +4,10 @@ Dr.Api = new function() {
     // Dr.Api.navReset();
     this.mainSet= function(){
         let mainPage =$("#contentsId");
-        $.get("./view/contents/_Band.html?check=" + Date.now(), function (data) {
+        $.get("./view/contents/_bandSection.html?check=" + Date.now(), function (data) {
             mainPage.append(data);
         });
-        $.get("./view/contents/_Tour.html?check=" + Date.now(), function (data) {
+        $.get("./view/contents/_tourSection.html?check=" + Date.now(), function (data) {
             mainPage.append(data);
         });
         
