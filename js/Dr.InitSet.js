90dc179b6e6a99b6609974611a5d1b243685e234
drhon9 <drhon9@naver.com>
2020-09-10 18:16:37 +0900

    revise main

--- js/Dr.InitSet.js
+++ js/Dr.InitSet.js
@@ -15,19 +15,36 @@ var Dr ={};
 		init : function() {
 
             that.htmlLoad();
-    
+   
         },
         htmlLoad: function() {
             $(document).ready(function () {
+
+                that.mainImage();
+                
                 $("div[data-includeHTML]").each(function () {                
                     $(this).load($(this).attr("data-includeHTML")); 
                 })
                 setTimeout(()=>{
                     Dr.Api.mainSet();
-                },1000)
+                },1000);
             });
         },
-  
+        mainImage: function() {
+            let imgTag = document.getElementById('mainImage');
+            let imgArray = [
+                "sa","mot","dw2","wa","webo","mix","mn","bioMain","rc","webp",
+                "delta","dreffect","cp2017","drs","drsaxophone_1","mn","reso","teles",
+            ];
+            let number = Math.floor(Math.random() * imgArray.length);
+            imgTag.src =`./img/${imgArray[number]}.jpg`
+
+            setInterval(() => {
+                let number = Math.floor(Math.random() * imgArray.length);
+                imgTag.src =`./img/${imgArray[number]}.jpg`
+            }, 2000);
+        },
+        
         preset :{
 				// load_json :function(){
 				// 	var jsondata = DrAJAX.LoadJsonData('./ajax/init.json'); //'./ajax/osc.xml'
